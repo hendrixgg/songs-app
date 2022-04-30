ba36dbfc54f27ca2299cc48378e1fad0a7ee52a7
hendrixgg <hendrixg10@gmail.com>
2022-04-30 15:15:52 -0400

    fixed App component render

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 
-ReactDOM.createRoot(document.querySelector("#root").render(<App />));
+ReactDOM.createRoot(document.querySelector("#root")).render(<App />);
